c44480b412818e0546df7ab32a1f3a4685e75aec
Johan F. <johan.artmat@gmail.com>
2023-10-31 10:30:17 -0500

    [UPDATE] implementation different query

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,44 @@ fn main() {
 
     let mut conn = PgConnection::establish(&db_url).expect("db connection failed");
 
-    use self::models::{Post, NewPost};
+    use self::models::{Post, NewPost, SpecificPost};
     use self::schema::posts;
     use self::schema::posts::dsl::*;
 
-    let new_post = NewPost{
-        title: "Mi primer blog",
-        body: "Lorem ipsum",
-        slug: "primer-post",
-    };
-    let _post: Post = diesel::insert_into(posts::table)
-        .values(new_post)
-        .get_result(&mut conn)
-        .expect("insert failed");
+    // let new_post = NewPost{
+    //     title: "Mi primer blog",
+    //     body: "Lorem ipsum",
+    //     slug: "primer-post",
+    // };
+    // let _post: Post = diesel::insert_into(posts::table)
+    //     .values(new_post)
+    //     .get_result(&mut conn)
+    //     .expect("insert failed");
 
     let mut post_result = posts.load::<Post>(&mut conn).expect("query error");
-
     for post in post_result {
-        println!("{}", post.title);
+        println!("{:?}", post);
     }
 
     post_result = posts.limit(1).load::<Post>(&mut conn).expect("query error");
-
     for post in post_result {
-        println!("{}", post.title);
+        println!("{:?}", post);
+    }
+
+    let specific_post_result = posts.select((title, body))
+        .load::<SpecificPost>(&mut conn)
+        .expect("query error");
+    for post in specific_post_result {
+        println!("{:?}", post);
+    }
+
+    post_result = posts.order(id.desc()).limit(1).load::<Post>(&mut conn).expect("query error");
+    for post in post_result {
+        println!("{:?}", post);
+    }
+
+    post_result = posts.filter(id.eq(2)).limit(1).load::<Post>(&mut conn).expect("query error");
+    for post in post_result {
+        println!("{:?}", post);
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,10 @@
+#[derive(Queryable, Debug)]
+pub struct SpecificPost {
+    pub title: String,
+    pub body: String,
+}
 
-#[derive(Queryable)]
+#[derive(Queryable, Debug)]
 pub struct Post {
     pub id: i32,
     pub title: String,
